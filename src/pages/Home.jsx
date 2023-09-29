f427896188fb9ea9e1b692d8a41011c6165d8898
luojiaxin <luojiaxin@yunquna.com>
2023-09-29 12:51:27 +0800

    fix:打包生产环境时需要配置

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import HomeContext from "../store/home";
 import styles from './index.module.scss';
@@ -7,8 +7,12 @@ export default function (props) {
 
     const store = useContext(HomeContext);
     console.log('%c [ store ]-9', 'font-size:13px; background:pink; color:#bf2c9f;', store)
+
+    const [num, setNum] = useState(0);
+
     const handleClick = () => {
-        console.log('handleClick')
+        console.log(num)
+        setNum(num + 1);
     };
 
     useEffect(() => {
@@ -17,8 +21,9 @@ export default function (props) {
     return (
         <div>
             <h1 className={styles.home}>Home</h1>
-            <img src="/wate.png" alt="" width={200} />
+            <img src="/wate.png" alt="hhh" width={200} />
             <button onClick={handleClick}>按钮</button>
+            <p>num is{num}</p>
             <br />
             <Link to="/some">some 页面</Link>
         </div>
